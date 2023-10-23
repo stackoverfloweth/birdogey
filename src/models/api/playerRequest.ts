f199075871b542ac249163a9fe176a84ba60bdb5
Evan Sutherland <stackoverfloweth@gmail.com>
2023-10-23 15:10:09 -0500

    fix player model objectid type

--- src/models/api/playerRequest.ts
+++ src/models/api/playerRequest.ts
@@ -1,6 +1,7 @@
 import { PlayerResponse } from '@/models/api/playerResponse'
 
-export type PlayerRequest = Omit<PlayerResponse, '_id' | 'tagId' | 'entryPaid'> & {
+export type PlayerRequest = Omit<PlayerResponse, '_id' | 'seasonId' | 'tagId' | 'entryPaid'> & {
+  seasonId: string,
   tagId?: number,
   entryPaid?: boolean,
 }
