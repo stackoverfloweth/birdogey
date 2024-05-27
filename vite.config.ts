d3ec9d8fd90f2bc8aabcd76bcab41401fac800db
Evan Sutherland <stackoverfloweth@gmail.com>
2024-05-27 15:52:38 -0500

    using new vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [vue(), svgLoader(), VueDevTools()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
