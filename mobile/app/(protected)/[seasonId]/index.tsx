2da1d665eb98fd090340ef10a6478044ff088c3f
Evan Sutherland <stackoverfloweth@gmail.com>
2026-04-01 10:22:15 -0500

    remove season-select from index

--- mobile/app/(protected)/[seasonId]/index.tsx
+++ mobile/app/(protected)/[seasonId]/index.tsx
@@ -1,22 +1,18 @@
 import { View, Text, StyleSheet } from 'react-native'
-import { router } from 'expo-router'
 import { useSeason } from '@/contexts/SeasonContext'
-import { useCallback } from 'react'
-import { SeasonSelectList } from '@/components/SeasonSelectList'
 
 export default function HomeScreen(): React.ReactNode {
-  const { selectedSeason, setSelectedSeasonId } = useSeason()
-
-  const navigateToSeason = useCallback((seasonId: string): void => {
-    setSelectedSeasonId(seasonId)
-    router.setParams({ seasonId })
-  }, [setSelectedSeasonId])
+  const { selectedSeason } = useSeason()
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Birdogey</Text>
-
-      <SeasonSelectList onSeasonPress={(season) => navigateToSeason(season.id)} selectedSeasonId={selectedSeason?.id} />
+      <Text>
+        {selectedSeason?.course.name}
+      </Text>
+      <Text>
+        {selectedSeason?.name}
+      </Text>
     </View>
   )
 }
