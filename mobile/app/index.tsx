4795bc53e5e1c45b0e9a2888e4cc9a80e978d3e7
Evan Sutherland <stackoverfloweth@gmail.com>
2026-04-16 11:03:54 -0500

    much of login screen rebuilt

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,22 +1,25 @@
+import { useAuth } from '@/contexts/AuthContext'
 import { Redirect } from 'expo-router'
 import { ActivityIndicator, View } from 'react-native'
 import { Text } from 'react-native'
 
 export default function Index(): React.ReactNode {
+  const { isAuthenticated, isLoading } = useAuth()
+
+  console.log({ isAuthenticated, isLoading })
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
+  if (!isAuthenticated) {
+    return <Redirect href="/login" />
+  }
+
   return <Text>Hello World</Text>
-  // const { isAuthenticated, isLoading } = useAuth()
-
-  // if (isLoading) {
-  //   return (
-  //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-  //       <ActivityIndicator size="large" />
-  //     </View>
-  //   )
-  // }
-
-  // if (!isAuthenticated) {
-  //   return <Redirect href="/login" />
-  // }
-
   // return <Redirect href="/(protected)" />
 }
---
+++ mobile/assets.d.ts
@@ -0,0 +1,10 @@
+declare module '*.png' {
+  import { ImageSourcePropType } from 'react-native'
+  const value: ImageSourcePropType
+  export default value
+}
+
+declare module '*.ttf' {
+  const value: number
+  export default value
+}
