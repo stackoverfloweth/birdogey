cebe79b19b818ca621b06f47721a759c37e7873a
Evan Sutherland <stackoverfloweth@gmail.com>
2026-04-18 10:29:04 -0500

    fix lint errors in AuthContext, separate ApiClientContext

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,14 +3,17 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '@/services/queryClient'
 import { AuthProvider } from '@/contexts/AuthContext'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { ApiClientProvider } from '@/contexts/ApiClientContext'
 
 export default function RootLayout(): React.ReactNode {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Stack screenOptions={{ headerShown: false }} />
-        </AuthProvider>
+        <ApiClientProvider>
+          <AuthProvider>
+            <Stack screenOptions={{ headerShown: false }} />
+          </AuthProvider>
+        </ApiClientProvider>
       </QueryClientProvider>
     </SafeAreaProvider>
   )
---
+++ mobile/src/contexts/ApiClientContext.tsx
@@ -0,0 +1,52 @@
+import { config } from '@/config/env'
+import { AuthApi, createUserApi, createAuthApi, createEventApi, createImageKitApi, EventApi, FetchHttpClient, ImageKitApi, UserApi } from '@birdogey/shared'
+import { createContext, useContext, useRef } from 'react'
+import { getAccessToken } from '@/services/tokenStorage'
+
+export type ApiClient = {
+  event: EventApi,
+  user: UserApi,
+  auth: AuthApi,
+  imagekit: ImageKitApi,
+}
+
+const ApiClientContext = createContext<ApiClient | null>(null)
+
+export function ApiClientProvider({ children }: { children: React.ReactNode }): React.ReactNode {
+  const apiClient = useRef<FetchHttpClient | null>(null)
+  apiClient.current ??= new FetchHttpClient({ baseUrl: config.apiBaseUrl, getAccessToken })
+
+  const auth = useRef<AuthApi | null>(null)
+  auth.current ??= createAuthApi(apiClient.current)
+
+  const user = useRef<UserApi | null>(null)
+  user.current ??= createUserApi(apiClient.current)
+
+  const event = useRef<EventApi | null>(null)
+  event.current ??= createEventApi(apiClient.current)
+
+  const imagekit = useRef<ImageKitApi | null>(null)
+  imagekit.current ??= createImageKitApi(apiClient.current)
+
+  return (
+    <ApiClientContext.Provider value={{
+      auth: auth.current,
+      user: user.current,
+      event: event.current,
+      imagekit: imagekit.current,
+    }}
+    >
+      {children}
+    </ApiClientContext.Provider>
+  )
+}
+
+export function useApiClient(): ApiClient {
+  const apiClient = useContext(ApiClientContext)
+
+  if (!apiClient) {
+    throw new Error('ApiClient not found')
+  }
+
+  return apiClient
+}
