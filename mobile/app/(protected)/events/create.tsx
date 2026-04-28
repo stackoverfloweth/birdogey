8a83dcab3eb0e9603b9edfa21c141c1f7832f8b0
Evan Sutherland <stackoverfloweth@gmail.com>
2026-04-28 10:46:14 -0500

    wire up EventForm to create event API

--- mobile/app/(protected)/events/create.tsx
+++ mobile/app/(protected)/events/create.tsx
@@ -1,10 +1,13 @@
 import { EventForm } from '@/components/EventForm'
 import { EventSeasonSelect } from '@/components/EventSeasonSelect'
 import { useApiClient } from '@/contexts/ApiClientContext'
+import { EventSchema } from '@birdogey/shared'
 import { formStyles } from '@/theme/forms'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useMemo, useState } from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { ScrollView } from 'react-native'
+import { queryClient } from '@/services/queryClient'
+import { router } from 'expo-router'
 
 export default function EventView(): React.ReactNode {
   const [seasonId, setSeasonId] = useState('')
@@ -13,6 +16,14 @@ export default function EventView(): React.ReactNode {
   const { data: events = [] } = useQuery({
     queryKey: ['events', seasonId],
     queryFn: () => api.event.getList(seasonId),
+  })
+
+  const { mutate: createEvent } = useMutation({
+    mutationFn: (data: EventSchema) => api.event.create({ seasonId, ...data }),
+    onSuccess: (eventId) => {
+      router.push(`/events/${eventId}`)
+      queryClient.invalidateQueries({ queryKey: ['events', seasonId] })
+    },
   })
 
   const latestEvent = useMemo(() => {
@@ -24,7 +35,7 @@ export default function EventView(): React.ReactNode {
   return (
     <ScrollView contentContainerStyle={formStyles.form}>
       <EventSeasonSelect value={seasonId} onChange={setSeasonId} />
-      {latestEvent && <EventForm key={seasonId} lastEvent={latestEvent} />}
+      {latestEvent && <EventForm key={seasonId} lastEvent={latestEvent} onSubmit={createEvent} />}
     </ScrollView>
   )
 }
--- mobile/src/components/EventForm.tsx
+++ mobile/src/components/EventForm.tsx
@@ -1,36 +1,34 @@
 import { formStyles } from '@/theme/forms'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Controller, useForm } from 'react-hook-form'
-import { View, ScrollView, Text, Pressable } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import { TextInput } from '@/components/TextInput'
-import { eventSchema, EventSchema, EventSchemaInput } from '@/schemas/eventSchema'
 import { SymbolView } from 'expo-symbols'
 import { format } from 'date-fns'
 import { useMemo } from 'react'
 import { router } from 'expo-router'
-import { calculateEventAcePotIfNoWinners, calculateEventCtpPotIfNoWinners, Event } from '@birdogey/shared'
+import { calculateEventAcePotIfNoWinners, calculateEventCtpPotIfNoWinners, Event, eventSchema, EventSchema, EventSchemaInput } from '@birdogey/shared'
+import { colors } from '@/theme/colors'
 
 export type EventFormProps = {
   lastEvent: Event | undefined,
+  onSubmit: (data: EventSchema) => void,
 }
 
-export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
+export function EventForm({ lastEvent, onSubmit }: EventFormProps): React.ReactNode {
   const today = format(new Date(), 'MMMM do')
 
   const previousEventBalance = useMemo(() => {
     return {
-      ctpPerPlayer: lastEvent?.ctpPerPlayer,
-      acePerPlayer: lastEvent?.acePerPlayer,
+      ctpPerPlayer: lastEvent?.ctpPerPlayer ? lastEvent.ctpPerPlayer / 100 : undefined,
+      acePerPlayer: lastEvent?.acePerPlayer ? lastEvent.acePerPlayer / 100 : undefined,
       ctpStartingBalance: lastEvent ? calculateEventCtpPotIfNoWinners(lastEvent) / 100 : undefined,
       aceStartingBalance: lastEvent ? calculateEventAcePotIfNoWinners(lastEvent) / 100 : undefined,
       ctpHole: lastEvent?.ctpHole,
     }
   }, [lastEvent])
 
-  console.log({ lastEvent })
-
-  const { control, handleSubmit, formState: { errors, isValid, isLoading, isSubmitted } } = useForm<EventSchemaInput, any, EventSchema>({
-    mode: 'onChange',
+  const { control, handleSubmit, formState: { errors, isLoading, isSubmitted } } = useForm<EventSchemaInput, any, EventSchema>({
     resolver: zodResolver(eventSchema),
     defaultValues: {
       name: today,
@@ -42,9 +40,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
     },
   })
 
-  async function onSubmit(data: EventSchema): Promise<void> {
-    console.log(data)
-  }
+  console.log({ errors })
 
   return (
     <>
@@ -95,6 +91,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
               value={value}
               style={formStyles.input}
               keyboardType="number-pad"
+              icon={<SymbolView name="dollarsign" size={20} tintColor={colors.primary} />}
             />
           )}
           name="ctpPerPlayer"
@@ -113,6 +110,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
               value={value}
               style={formStyles.input}
               keyboardType="number-pad"
+              icon={<SymbolView name="dollarsign" size={20} tintColor={colors.primary} />}
             />
           )}
           name="ctpStartingBalance"
@@ -149,6 +147,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
               value={value}
               style={formStyles.input}
               keyboardType="number-pad"
+              icon={<SymbolView name="dollarsign" size={20} tintColor={colors.primary} />}
             />
           )}
           name="acePerPlayer"
@@ -167,6 +166,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
               value={value}
               style={formStyles.input}
               keyboardType="number-pad"
+              icon={<SymbolView name="dollarsign" size={20} tintColor={colors.primary} />}
             />
           )}
           name="aceStartingBalance"
@@ -176,7 +176,7 @@ export function EventForm({ lastEvent }: EventFormProps): React.ReactNode {
 
       <Pressable
         disabled={isLoading}
-        style={[formStyles.button, !isValid && formStyles.buttonDisabled]}
+        style={formStyles.button}
         onPress={() => void handleSubmit(onSubmit)()}
       >
         <Text style={formStyles.buttonText}>Create Event</Text>
